b8f3dd729fc3da859337108685aecbffa13c9eea
Axel Barlet <axelbarlet98@gmail.com>
2022-06-22 00:56:10 +0200

    feat: Update Node rules to run dev-server and other rules in same time

--- app/node.mk
+++ app/node.mk
@@ -1,9 +1,13 @@
 ### Node rules
 
+node_ports=
+node_container_name="$(stack_name)_node"
+node_exec_params="-it --rm --name $(node_container_name) $(ports) -v $(shell pwd)/app:/usr/src/app -w /usr/src/app"
+
 ### Default rule
 node.default:
 	make docker.run \
-		exec_params="-it --rm --name $(stack_name)_node -p 8081:8081/tcp -v $(shell pwd)/app:/usr/src/app -w /usr/src/app" \
+		exec_params=$(node_exec_params) \
 		docker_image="node:18" \
 		container_command="$(container_command)"
 
@@ -11,8 +15,11 @@ node.default:
 
 ### Yarn rules
 yarn:
-	make node.default container_command=yarn
+	make node.default container_command="yarn $(args)"
 
 yarn.dev-server:
 	make yarn
-	make node.default container_command="yarn dev-server"
+	make node.default \
+ 		container_command="yarn dev-server" \
+ 		node_container_name="$(node_container_name)_dev_server" \
+		ports="-p 8081:8081/tcp"
