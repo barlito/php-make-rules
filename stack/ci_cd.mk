f2ffc7039a404084c11c2edccb355e3a9e720633
Axel Barlet <axelbarlet16@gmail.com>
2023-03-18 16:55:53 +0100

    feat: Update deploy.ci

--- stack/ci_cd.mk
+++ stack/ci_cd.mk
@@ -6,6 +6,6 @@ deploy.ci:
 	make composer.command args="config -g github-oauth.github.com $(args)"
 	make composer.install
 	make docker.command exec_params="-t" args="chmod +x bin/console"
-	make doctrine.migrate
-	make doctrine.load_fixtures
+	make doctrine.migrate.ci
+	make doctrine.load_fixtures.ci
 	#make symfony.security_check
