9421d58638082f4c6f10d35f3ab48d6fe85650fe
Axel Barlet <axelbarlet16@gmail.com>
2023-08-13 05:13:01 +0200

    feat: Delete permissions rules

--- stack/_entrypoint.mk
+++ stack/_entrypoint.mk
@@ -3,6 +3,3 @@ include make/stack/docker.mk
 
 # Deployment rules
 include make/stack/deployment.mk
-
-# Permissions rules
-include make/stack/permissions.mk
--- stack/deployment.mk
+++ stack/deployment.mk
@@ -27,13 +27,11 @@ deploy.ci:
 deploy.prod:
 	make docker.deploy.prod
 	make docker.wait_stack
-	make permissions
 	make doctrine.migrate
 
 update.service:
 	make docker.service.update args="$(service_update_args)"
 	make docker.wait_stack
-	make permissions
 	make doctrine.migrate
 
 undeploy:
