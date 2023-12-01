1bd9ef64cf4d6968cf5a08bbfe7fb3bc5a399f3f
Axel Barlet <axelbarlet16@gmail.com>
2023-12-01 23:42:44 +0100

    feat: Update symfony install + update security check

--- app/symfony.mk
+++ app/symfony.mk
@@ -2,8 +2,9 @@
 
 symfony.install:
 	rm -rf app/.gitkeep
-	docker exec -t $(app_container_id) bash -c "composer create-project symfony/skeleton ./"
+	docker exec -t $(app_container_id) bash -c "composer create-project symfony/skeleton ./app"
+	mv app/* ./ && mv app/.* ./ && rm -rf app
 
 symfony.security_check:
-	docker run --rm -v $(shell pwd):$(shell pwd) -w $(shell pwd)/app symfonycorp/cli security:check
+	docker run --rm -v ./:/app -w /app ghcr.io/symfony-cli/symfony-cli security:check
 
