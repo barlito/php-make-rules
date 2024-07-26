4ea36b0fab22777640c127c40bbf04f26a751717
Axel Barlet <axelbarlet16@gmail.com>
2024-07-26 17:33:45 +0200

    fix: Fix docker wait container castor command

--- stack/docker.php
+++ stack/docker.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Barlito\Castor;
 
 use Castor\Attribute\AsTask;
@@ -10,7 +9,7 @@ use function Castor\wait_for_docker_container;
 #[AsTask('wait-nginx-container')]
 function waitNginxContainer(): void {
     wait_for_docker_container(
-        containerName: '$STACK_NAME_nginx',
+        containerName: '${STACK_NAME}_php',
 //        portsToCheck: [80, 443],
         message: 'Waiting for my-container to be ready...',
     );
