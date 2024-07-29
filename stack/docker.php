af9b70b1a38ce53279dabd2a192174d90609ebb1
Axel Barlet <axelbarlet16@gmail.com>
2024-07-29 23:52:45 +0200

    feat: Enhance docker wait container

--- stack/docker.php
+++ stack/docker.php
@@ -3,23 +3,31 @@
 namespace Barlito\Castor;
 
 use Castor\Attribute\AsTask;
-use function Castor\io;
 use Castor\Exception\WaitFor\DockerContainerStateException;
 
+use function Castor\io;
+use function Castor\context;
 use function Castor\wait_for_docker_container;
 
 #[AsTask('wait-php-container')]
-function waitNginxContainer(): void
+function waitNginxContainer($loopCount = 0): void
 {
+    $context = context();
+    $STACK_NAME = $context->environment['STACK_NAME'];
+
     try {
         wait_for_docker_container(
             containerName: '${STACK_NAME}_php',
 //          portsToCheck: [80, 443],
-            message: 'Waiting for Container to be ready...',
+            message: "Waiting for Container {$STACK_NAME}_php to be ready...",
         );
-    } catch (DockerContainerStateException) {
-        io()->info('Waiting for Container to start...');
+    } catch (DockerContainerStateException $exception) {
+        if($loopCount > 60) {
+            throw $exception;
+        }
+
+        io()->info("Waiting for Container {$STACK_NAME}_php to be ready..." . $loopCount);
         sleep(1);
-        waitNginxContainer();
+        waitNginxContainer($loopCount + 1);
     }
 }
