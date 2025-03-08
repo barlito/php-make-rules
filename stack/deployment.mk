a915968d8cbe77a9d40b6d4945e51cd99e4a1cf7
Axel Barlet <axelbarlet16@gmail.com>
2025-03-08 23:13:49 +0100

    feat: Add wait for DB container

--- stack/deployment.mk
+++ stack/deployment.mk
@@ -8,6 +8,7 @@ deploy:
 	make set_file_permissions
 	make docker.deploy
 	castor barlito:castor:wait-php-container
+	castor barlito:castor:wait-db-container
 	make composer.install
 	make doctrine.migrate
 	make doctrine.load_fixtures
@@ -17,6 +18,7 @@ deploy:
 deploy.ci:
 	make docker.deploy.ci
 	castor barlito:castor:wait-php-container
+	castor barlito:castor:wait-db-container
 	make composer.command args="config -g github-oauth.github.com $(github_token)"
 	make composer.install
 	make docker.command exec_params="-t" args="chmod +x bin/console"
@@ -28,6 +30,7 @@ deploy.ci:
 deploy.prod:
 	make docker.deploy.prod
 	castor barlito:castor:wait-php-container
+	castor barlito:castor:wait-db-container
 	make doctrine.migrate
 
 update.service:
--- stack/docker.php
+++ stack/docker.php
@@ -8,6 +8,7 @@ use Castor\Exception\WaitFor\DockerContainerStateException;
 use function Castor\io;
 use function Castor\context;
 use function Castor\wait_for_docker_container;
+use function Castor\run;
 
 #[AsTask('wait-php-container')]
 function waitNginxContainer($loopCount = 0): void
@@ -29,5 +30,34 @@ function waitNginxContainer($loopCount = 0): void
         io()->info("Waiting for Container {$STACK_NAME}_php to be ready..." . $loopCount);
         sleep(1);
         waitNginxContainer($loopCount + 1);
+    }
+}
+
+#[AsTask('wait-db-container')]
+function waitDatabaseContainer($dbName = null, $loopCount = 0): void
+{
+    $context = context();
+    $STACK_NAME = $context->environment['STACK_NAME'];
+    $DB_NAME = $context->environment['DB_NAME'];
+    if ($dbName === null) {
+        $dbName = $DB_NAME;
+    }
+
+    try {
+        wait_for_docker_container(
+            containerName: '${STACK_NAME}_db',
+            message: "Waiting for Container {$STACK_NAME}_db to be ready...",
+            containerChecker: function ($containerId) use ($dbName) {
+                return run("docker exec $containerId pg_isready -d " . $dbName, context: context()->withAllowFailure())->isSuccessful();
+            },
+        );
+    } catch (DockerContainerStateException $exception) {
+        if ($loopCount > 60) {
+            throw $exception;
+        }
+
+        io()->info("Waiting for Container {$STACK_NAME}_db to be ready..." . $loopCount);
+        sleep(1);
+        waitDatabaseContainer($dbName, $loopCount + 1);
     }
 }
